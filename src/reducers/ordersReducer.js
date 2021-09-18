1a69af1ae60b62ad7047f36f6b11d1f82d77866d
Emmanuel Adiba <eabaagah@gmail.com>
2021-09-18 14:22:51 +0000

    reducers setup

---
+++ src/reducers/ordersReducer.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { FETCH_ORDERS } from "../constants";
+
+export default (state =[], action) => {
+    switch (action.type) {
+        case FETCH_ORDERS:
+            return state
+    
+        default:
+            return state
+    }
+}
