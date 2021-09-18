d4676e1b4d474495fe09efb2e93af4aa5fe67ca7
Emmanuel Adiba <eabaagah@gmail.com>
2021-09-18 14:17:25 +0000

    set boilerplate

---
+++ src/api/index.js
@@ -0,0 +1,3 @@
+import axios from 'axios'
+
+export const orderSummary = axios.get('https://indapi.kumba.io/webdev/assignment')
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const FETCH_ORDERS = 'FETCH_ORDERS'
+export const GET_USER = 'GET_USER'
