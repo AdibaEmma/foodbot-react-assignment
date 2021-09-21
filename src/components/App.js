9572a560c4932676d20b83734c68c3fc49d1f7ca
Emmanuel Adiba <eabaagah@gmail.com>
2021-09-21 15:26:07 +0000

    cleanup update

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App({ order, order_details }) {
   useEffect(() => {
     order_details()
   }, [order_details])
-console.log(order);
+
   return (
     <div className="container mt-3 bg-white">
       <BrowserRouter>
